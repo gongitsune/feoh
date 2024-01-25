d4dfd278e1dcff6afed643ee3adcc4c76dd4aad7
kara <89068705+gongitsune@users.noreply.github.com>
2024-01-25 12:06:53 +0900

    feat: output image

--- other-renderer/examples/main.rs
+++ other-renderer/examples/main.rs
@@ -1,5 +1,13 @@
 use anyhow::{Ok, Result};
 
 fn main() -> Result<()> {
+    let info = other_renderer::RenderInfo {
+        img_width: 256,
+        img_height: 256,
+    };
+    let img = other_renderer::output(&info)?;
+
+    img.save("dist/output.png")?;
+
     Ok(())
 }
--- other-renderer/src/lib.rs
+++ other-renderer/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::{Ok, Result};
+use image::{DynamicImage, RgbImage};
+
+pub struct RenderInfo {
+    pub img_width: usize,
+    pub img_height: usize,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn render(info: &RenderInfo, pixels: &mut [u8]) -> Result<()> {
+    let img_width = info.img_width;
+    let img_height = info.img_height;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for j in (0..img_height - 1).rev() {
+        for i in 0..img_width {
+            let r = i as f64 / (img_width - 1) as f64;
+            let g = j as f64 / (img_height - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            let pix_idx = 3 * (j * img_width + i);
+            pixels[pix_idx] = ir;
+            pixels[pix_idx + 1] = ig;
+            pixels[pix_idx + 2] = ib;
+        }
     }
+
+    Ok(())
+}
+
+pub fn output(info: &RenderInfo) -> Result<DynamicImage> {
+    let mut pixels = vec![0; 3 * info.img_width * info.img_height];
+    render(info, &mut pixels)?;
+
+    let image = RgbImage::from_raw(info.img_width as u32, info.img_height as u32, pixels)
+        .expect("image creation failed");
+
+    Ok(DynamicImage::ImageRgb8(image))
 }
